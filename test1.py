5f4b467a703bee98be90dcd77d14409d7e4f4ffc
XiaoZhuGor <144261199+XiaoZhuGor@users.noreply.github.com>
2023-09-07 00:24:55 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import joblib
+
+# Create a file uploader to allow users to upload the model file
+model_file = st.file_uploader("Upload model file (.pkl)", type=["pkl"])
+
+# Check if a model file was uploaded
+if model_file:
+    # Load the model from the uploaded file
+    loaded_models = joblib.load(model_file)
+
+    # Define the Streamlit app
+    st.title("Streamlit App with Multiple Models")
+
+    # Create a dropdown to select the model
+    selected_model = st.selectbox("Select a Model", list(loaded_models.keys()))
+
+    # Create an input field for user input (you can modify this based on your model's input requirements)
+    user_input = st.text_input("Enter some text:", "")
+
+    # Function to make predictions using the selected model
+    def make_prediction(input_text, selected_model):
+        model = loaded_models[selected_model]
+        # Replace this with your actual prediction code for the selected model
+        prediction = model.predict([input_text])[0]
+        return prediction
+
+    # Make predictions when the user clicks a button
+    if st.button("Make Prediction"):
+        if user_input:
+            prediction = make_prediction(user_input, selected_model)
+            st.write(f"Prediction using {selected_model}: {prediction}")
+
+# Note: You should replace the prediction logic with your specific model's prediction code.
