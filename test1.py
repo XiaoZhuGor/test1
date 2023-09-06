1bf6c37fe07d1f78cbb393c556728dfe9a72ec38
XiaoZhuGor <144261199+XiaoZhuGor@users.noreply.github.com>
2023-09-07 01:14:55 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,41 +1,79 @@
-
 import streamlit as st
 import joblib
+import re
+import string
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import nltk
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from scipy.sparse import hstack
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
 
-# Create a file uploader to allow users to upload the model file
-model_file = st.file_uploader("Upload model file (.pkl)", type=["pkl"])
+# Load your pre-trained model (model1)
+model1 = joblib.load("tolonglah.pkl")
 
-# Check if a model file was uploaded
-if model_file:
-    # Load the model from the uploaded file
-    loaded_models = joblib.load(model_file)
+# Define the text preprocessing function
+def preprocess_text(text):
+    # Convert text to lowercase
+    text = text.lower()
 
-    # Define the Streamlit app
-    st.title("Streamlit App with Multiple Models")
+    # Remove numbers and special characters using regular expressions
+    text = re.sub(r'[^a-zA-Z]', ' ', text)
 
-    # Create a dropdown to select the model
-    selected_model = st.selectbox("Select a Model", list(loaded_models.keys()))
+    # Remove extra whitespaces
+    text = ' '.join(text.split())
 
-    # Create an input field for user input
-    user_input = st.text_input("Enter some text:", "")
+    # Remove stopwords
+    stop_words = set(stopwords.words('english'))
 
-    # Load the vectorizer used during training (replace 'vectorizer' with your actual vectorizer)
-    vectorizer = joblib.load("your_vectorizer.pkl")  # Replace with the path to your vectorizer
+    # Split the text into sentences
+    sentences = re.split(r'[.!?]', text)
 
-    # Function to make predictions using the selected model
-    def make_prediction(input_text, selected_model):
-        model = loaded_models[selected_model]
+    # Process each sentence
+    cleaned_sentences = []
+    for sentence in sentences:
+        words = sentence.split()
 
-        # Vectorize the input text using the same vectorizer used during training
-        input_features = vectorizer.transform([input_text])
+        # Remove the first word in the sentence
+        if len(words) > 1:
+            words = words[1:]
 
-        prediction = model.predict(input_features)[0]
-        return prediction
+        # Remove stopwords and apply stemming
+        words = [word for word in words if word not in stop_words]
 
-    # Make predictions when the user clicks a button
-    if st.button("Make Prediction"):
-        if user_input:
-            prediction = make_prediction(user_input, selected_model)
-            st.write(f"Prediction using {selected_model}: {prediction}")
+        # Apply stemming
+        stemmer = PorterStemmer()
+        stemmed_words = [stemmer.stem(word) for word in words]
+
+        # Join stemmed words back into a sentence
+        cleaned_sentence = ' '.join(stemmed_words)
+        cleaned_sentences.append(cleaned_sentence)
+
+    # Join the cleaned sentences back together
+    cleaned_text = ' '.join(cleaned_sentences)
+
+    return cleaned_text
+
+# Create a file uploader to allow users to upload text data
+user_input = st.text_area("Enter some text:", "")
+
+# Preprocess the user input
+if user_input:
+    preprocessed_input = preprocess_text(user_input)
+
+    # Vectorize the preprocessed input using TF-IDF and BoW
+    tfidf_vectorizer = TfidfVectorizer(max_features=10000, ngram_range=(1, 2))
+    bow_vectorizer = CountVectorizer(max_features=10000, ngram_range=(1, 2))
+    
+    # Transform the preprocessed input
+    tfidf_features = tfidf_vectorizer.transform([preprocessed_input])
+    bow_features = bow_vectorizer.transform([preprocessed_input])
+
+    # Combine TF-IDF and BoW features
+    combined_features = hstack([tfidf_features, bow_features])
+
+    # Make predictions using model1
+    prediction = model1.predict(combined_features)[0]
+
+    # Display the prediction result
+    st.write(f"Prediction: {prediction}")
