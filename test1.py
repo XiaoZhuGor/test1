fcfa37fb043991230630be4abe7951d145e9d7b6
XiaoZhuGor <144261199+XiaoZhuGor@users.noreply.github.com>
2023-09-07 13:59:31 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -10,45 +10,94 @@ import pandas as pd
 
 # Create a function to preprocess text
 def preprocess_text(text):
-    # Convert text to lowercase
+    def remove_stopwords(text):
+        text = ' '.join([word for word in text.split() if word not in stopwords.words('english')])
+        return text
+
+    def remove_url(text):
+        url = re.compile(r'https?://\S+|www\.\S+')
+        return url.sub(r'', text)
+
+    def remove_punct(text):
+        table = str.maketrans('', '', string.punctuation)
+        return text.translate(table)
+
+    def remove_html(text):
+        html = re.compile(r'<.*?>')
+        return html.sub(r'', text)
+
+    def remove_username(text):
+        return re.sub('@[^\s]+', '', text)
+
+    def remove_emoji(text):
+        emoji_pattern = re.compile("["
+                                    u"\U0001F600-\U0001F64F"  # emoticons
+                                    u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                    u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                    u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                    u"\U00002702-\U000027B0"
+                                    u"\U000024C2-\U0001F251"
+                                    "]+", flags=re.UNICODE)
+        return emoji_pattern.sub(r'', text)
+
+    def decontraction(text):
+        text = re.sub(r"won\'t", " will not", text)
+        text = re.sub(r"won\'t've", " will not have", text)
+        text = re.sub(r"can\'t", " can not", text)
+        text = re.sub(r"don\'t", " do not", text)
+
+        text = re.sub(r"can\'t've", " can not have", text)
+        text = re.sub(r"ma\'am", " madam", text)
+        text = re.sub(r"let\'s", " let us", text)
+        text = re.sub(r"ain\'t", " am not", text)
+        text = re.sub(r"shan\'t", " shall not", text)
+        text = re.sub(r"sha\n't", " shall not", text)
+        text = re.sub(r"o\'clock", " of the clock", text)
+        text = re.sub(r"y\'all", " you all", text)
+        text = re.sub(r"n\'t", " not", text)
+        text = re.sub(r"n\'t've", " not have", text)
+        text = re.sub(r"\'re", " are", text)
+        text = re.sub(r"\'s", " is", text)
+        text = re.sub(r"\'d", " would", text)
+        text = re.sub(r"\'d've", " would have", text)
+        text = re.sub(r"\'ll", " will", text)
+        text = re.sub(r"\'ll've", " will have", text)
+        text = re.sub(r"\'t", " not", text)
+        text = re.sub(r"\'ve", " have", text)
+        text = re.sub(r"\'m", " am", text)
+        text = re.sub(r"\'re", " are", text)
+        return text
+
+    def separate_alphanumeric(text):
+        words = text
+        words = re.findall(r"[^\W\d_]+|\d+", words)
+        return " ".join(words)
+
+    def cont_rep_char(text):
+        tchr = text.group(0)
+
+        if len(tchr) > 1:
+            return tchr[0:2]
+
+    def unique_char(rep, text):
+        substitute = re.sub(r'(\w)\1+', rep, text)
+        return substitute
+
+    def char(text):
+        substitute = re.sub(r'[^a-zA-Z]', ' ', text)
+        return substitute
+
+    text = remove_username(text)
+    text = remove_url(text)
+    text = remove_emoji(text)
+    text = decontraction(text)
+    text = separate_alphanumeric(text)
+    text = unique_char(cont_rep_char, text)
+    text = char(text)
     text = text.lower()
+    text = remove_stopwords(text)
 
-    # Remove numbers and special characters using regular expressions
-    text = re.sub(r'[^a-zA-Z]', ' ', text)
-
-    # Remove extra whitespaces
-    text = ' '.join(text.split())
-
-    # Remove stopwords
-    stop_words = set(stopwords.words('english'))
-
-    # Split the text into sentences
-    sentences = re.split(r'[.!?]', text)
-
-    # Process each sentence
-    cleaned_sentences = []
-    for sentence in sentences:
-        words = sentence.split()
-
-        # Remove the first word in the sentence
-        if len(words) > 1:
-            words = words[1:]
-
-        # Remove stopwords and apply stemming
-        words = [word for word in words if word not in stop_words]
-
-        # Apply stemming
-        stemmer = PorterStemmer()
-        stemmed_words = [stemmer.stem(word) for word in words]
-
-        # Join stemmed words back into a sentence
-        cleaned_sentence = ' '.join(stemmed_words)
-        cleaned_sentences.append(cleaned_sentence)
-
-    # Join the cleaned sentences back together
-    cleaned_text = ' '.join(cleaned_sentences)
-
-    return cleaned_text
+    return text
 
 # Load your pre-trained model (model1)
 model1 = joblib.load("tolonglah.pkl")
