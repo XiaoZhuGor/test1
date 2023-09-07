3e8d5bf3f8aefcae4b6d6a1c9548de7cb4ab4302
XiaoZhuGor <144261199+XiaoZhuGor@users.noreply.github.com>
2023-09-07 11:20:25 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,8 +6,11 @@ from nltk.stem import PorterStemmer
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
-import numpy as np
 
+# Download NLTK stopwords
+nltk.download('stopwords')
+
+# Create a function to preprocess text
 def preprocess_text(text):
     # Convert text to lowercase
     text = text.lower()
@@ -29,6 +32,10 @@ def preprocess_text(text):
     for sentence in sentences:
         words = sentence.split()
 
+        # Remove the first word in the sentence
+        if len(words) > 1:
+            words = words[1:]
+
         # Remove stopwords and apply stemming
         words = [word for word in words if word not in stop_words]
 
@@ -48,20 +55,42 @@ def preprocess_text(text):
 # Load your pre-trained model (model1)
 model1 = joblib.load("tolonglah.pkl")
 
+# Load your CSV data into a DataFrame
+data = pd.read_csv('Tweets.csv', encoding='latin1')
+# Apply preprocessing to the 'text' column using .apply()
+data['cleaned_data'] = data['text'].apply(preprocess_text)
+
 # Create a Streamlit app
 st.title("deployment test")
 
 # Create a text input field
 user_input = st.text_area("Enter some text:", "")
 
-# Create a button to make predictions
+
+# Recreate the TF-IDF vectorizer with the same parameters used during training
+tfidf_vectorizer = TfidfVectorizer(max_features=20032, ngram_range=(1, 2))
+tfidf_features = tfidf_vectorizer.fit_transform(data['cleaned_data'])
+
+# Recreate the BoW vectorizer with the same parameters used during training
+bow_vectorizer = CountVectorizer(max_features=20032, ngram_range=(1, 2))
+bow_features = bow_vectorizer.fit_transform(data['cleaned_data'])
+
 if st.button("Make Prediction"):
     if user_input:
-         # Preprocess the user input
+        # Preprocess the user input
         preprocessed_input = preprocess_text(user_input)
 
+        # Transform the preprocessed input using the same TF-IDF vectorizer
+        tfidf_input = tfidf_features.transform([preprocessed_input])
+
+        # Transform the preprocessed input using the same BoW vectorizer
+        bow_input = bow_features.transform([preprocessed_input])
+
+        # Combine the TF-IDF and BoW features for prediction
+        combined_input = hstack([tfidf_input, bow_input])
+
         # Make predictions using model1
-        prediction = model1.predict([preprocessed_input.split()])  # Pass a list of lists
+        prediction = model1.predict(combined_input)
 
         # Display the prediction result
         st.write(f"Prediction: {prediction[0]}")
