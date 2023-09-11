1e6e9abc742f10105626cba5edc01c7641ebee8d
XiaoZhuGor <144261199+XiaoZhuGor@users.noreply.github.com>
2023-09-12 01:40:24 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -120,7 +120,7 @@ st.title("Deployment Test")
 user_input = st.text_area("Enter some text:", "")
 
 # Recreate the TF-IDF vectorizer with the same parameters used during training
-tfidf_vectorizer = TfidfVectorizer(max_features=2500, ngram_range=(1, 3), max_df=0.25)
+tfidf_vectorizer = TfidfVectorizer(max_features=5000, ngram_range=(1, 3), max_df=0.5)
 tfidf_features = tfidf_vectorizer.fit_transform(data['cleaned_data'])
 
 # Create a button to make predictions
