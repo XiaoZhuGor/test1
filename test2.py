7f90da1edcce09360eaf72cb815d698bedb21b3a
XiaoZhuGor <144261199+XiaoZhuGor@users.noreply.github.com>
2023-09-15 18:28:41 +0800

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,8 +7,6 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 import pandas as pd
 import subprocess
-
-# Import matplotlib after installation
 import matplotlib.pyplot as plt
 
 # Check if NLTK stopwords are already downloaded
@@ -122,46 +120,104 @@ st.title("Sentiment Analysis Test")
 
 # Create a form to encapsulate the input fields
 with st.form("text_prediction_form"):
+    # Create a selectbox to allow the user to choose the model
+    prediction_type = st.selectbox("Select a prediction type", ["Text Prediction", "File Prediction"])
+    
     # Create a text input field
     user_input = st.text_area("Enter Text Prediction Here:", "")
 
     # Create a selectbox to allow the user to choose the model
     selected_model = st.selectbox("Select a Model", ["BernoulliNB", "LinearSVC"])
 
+    #provide uplaod file platform
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+    
     # Create a button to make predictions
     prediction_button = st.form_submit_button("Make Prediction")
+
+    
 
 
 # Check if the form is submitted
 if prediction_button:
-    if user_input:
-        if selected_model == "BernoulliNB":
-            # Preprocess the user input for TF-IDF and BoW features
-            preprocessed_input = preprocess_input_text(user_input)
-
-            # Transform the preprocessed input using the same TF-IDF vectorizer
-            tfidf_input = tfidf_vectorizer.transform([preprocessed_input])
-
-            # Make predictions using model1
-            prediction = model1.predict(tfidf_input)
-        
-            # Display the preprocessed input
-            st.write(f"Preprocessed text: {preprocessed_input}")
-
-            # Display the prediction result
-            st.write(f"Prediction: {prediction}")
+    if  prediction_type == "Text Prediction":
+        if not user_input:
+             st.error("Please enter text for prediction.")
         else:
-            # Preprocess the user input for TF-IDF and BoW features
-            preprocessed_input = preprocess_input_text(user_input)
-
-            # Transform the preprocessed input using the same TF-IDF vectorizer
-            tfidf_input = tfidf_vectorizer2.transform([preprocessed_input])
+          if selected_model == "BernoulliNB":
+                # Preprocess the user input for TF-IDF and BoW features
+                preprocessed_input = preprocess_input_text(user_input)
+    
+                # Transform the preprocessed input using the same TF-IDF vectorizer
+                tfidf_input = tfidf_vectorizer.transform([preprocessed_input])
+    
+                # Make predictions using model1
+                prediction = model1.predict(tfidf_input)
             
-            # Make predictions using model2
-            prediction = model2.predict(tfidf_input)
-
-            # Display the preprocessed input
-            st.write(f"Preprocessed text: {preprocessed_input}")
-
-            # Display the prediction result
-            st.write(f"Prediction: {prediction}")
+                # Display the preprocessed input
+                st.write(f"Preprocessed text: {preprocessed_input}")
+    
+                # Display the prediction result
+                st.write(f"Prediction: {prediction}")
+            else:
+                # Preprocess the user input for TF-IDF and BoW features
+                preprocessed_input = preprocess_input_text(user_input)
+    
+                # Transform the preprocessed input using the same TF-IDF vectorizer
+                tfidf_input = tfidf_vectorizer2.transform([preprocessed_input])
+                
+                # Make predictions using model2
+                prediction = model2.predict(tfidf_input)
+    
+                # Display the preprocessed input
+                st.write(f"Preprocessed text: {preprocessed_input}")
+    
+                # Display the prediction result
+                st.write(f"Prediction: {prediction}")
+    else:
+        if not uploaded_file:
+                st.error("Please upload a CSV file for prediction.")
+        else:
+            if selected_model == "BernoulliNB":
+                data = pd.read_csv(uploaded_file, encoding='latin1')
+                
+                # Apply preprocessing to the 'text' column using .apply()
+                data['cleaned_data'] = data['text'].apply(preprocess_input_text)
+                 
+                tfidf_features = tfidf_vectorizer.transform(data['cleaned_data'])
+                
+                # Make predictions using model1
+                predictions = model1.predict(tfidf_features)
+                
+                prediction_counts = pd.Series(predictions).value_counts()
+                plt.figure(figsize=(8, 6))
+                plt.bar(prediction_counts.index, prediction_counts.values, tick_label=['Neutral', 'Positive', 'Negative'])
+                plt.xlabel("Sentiment")
+                plt.ylabel("Count")
+                plt.title("Sentiment Analysis Results")
+                plt.ylim(0, 15000)  # Set the Y-axis limit to 1000 per inch
+                st.pyplot(plt)
+            
+            else:
+                data = pd.read_csv(uploaded_file, encoding='latin1')
+                
+                # Apply preprocessing to the 'text' column using .apply()
+                data['cleaned_data'] = data['text'].apply(preprocess_input_text)
+                 
+                tfidf_features = tfidf_vectorizer2.transform(data['cleaned_data'])
+                
+                # Make predictions using model1
+                predictions = model2.predict(tfidf_features)
+                
+                prediction_counts = pd.Series(predictions).value_counts()
+                plt.figure(figsize=(8, 6))
+                plt.bar(prediction_counts.index, prediction_counts.values, tick_label=['Neutral', 'Positive', 'Negative'])
+                plt.xlabel("Sentiment")
+                plt.ylabel("Count")
+                plt.title("Sentiment Analysis Results")
+                plt.ylim(0, 15000)  # Set the Y-axis limit to 1000 per inch
+                st.pyplot(plt)
+                
+            
+  
+     
